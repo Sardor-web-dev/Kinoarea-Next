440e7f2ccffab7f65b4fc9afe9b14a87d56d96f4
SarikViper <dzamolovsardor5@gmail.com>
2025-04-14 18:21:19 +0500

    last changes

--- src/pages/person/[id].tsx
+++ src/pages/person/[id].tsx
@@ -4,17 +4,22 @@ import { Person } from "@/types/actor";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useState } from "react";
 
 const PersonPage = ({ person }: { person: Person }) => {
-  const media = [...(person.combined_credits.cast || [])]
-    .sort((a, b) => (a.popularity || 0) - (b.popularity || 0))
-    .reverse();
+  const media = [...(person.combined_credits.cast || [])].sort(
+    (a, b) => (b.popularity || 0) - (a.popularity || 0)
+  );
 
-    
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const loadMore = () => {
+    setVisibleCount((prev) => prev + 8);
+  };
+
   return (
     <div className="w-full bg-[#1e2538]">
-      <div className="min-h-screen bg-[#1c2538] max-w-[1200px] mx-auto text-white px-4 sm:px-6 lg:px-8">
+      <div className="min-h-screen bg-[#1e2538] max-w-[1200px] mx-auto text-white px-4 sm:px-6 lg:px-8">
         <Header />
 
         <div className="flex flex-col md:flex-row gap-8 md:gap-12 mt-10">
@@ -50,8 +55,9 @@ const PersonPage = ({ person }: { person: Person }) => {
           <h2 className="text-2xl sm:text-3xl font-semibold mb-6">
             Фильмография
           </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6">
-            {media.map((item) => (
+
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 sm:gap-6">
+            {media.slice(0, visibleCount).map((item) => (
               <div
                 key={item.id}
                 className="bg-zinc-800 rounded-xl overflow-hidden shadow-md"
@@ -88,6 +94,17 @@ const PersonPage = ({ person }: { person: Person }) => {
               </div>
             ))}
           </div>
+
+          {visibleCount < media.length && (
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={loadMore}
+                className="px-6 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-lg transition duration-300"
+              >
+                Показать ещё
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
